1ad7791836562f2caa899e34a021defd5ea78ec8
Magnus Gud <maggu@gmail.com>
2016-03-18 14:39:03 +0100

    sataniagatan

--- bin/www.js
+++ bin/www.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 var app = require('../app');
-var debug = require('debug')('recRocker:server');
+var debug = require('debug')('adv1:server');
 var http = require('http');
 console.log("HEH HOH");
 /**
  * Get port from environment and store in Express.
  */
-var port = normalizePort(process.env.PORT || '3000');
+var port = process.env.port || 3000;
 app.set('port', port);
 /**
  * Create HTTP server.
@@ -18,21 +18,8 @@ var server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-    return false;
-}
+
+
 /**
  * Event listener for HTTP server "error" event.
  */
